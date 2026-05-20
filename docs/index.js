84f5179fa48e9199d22c9ecc6f3f197499c9862f
Samuel Castillo <samuelcastillo@MacBook-de-Samuel.local>
2026-05-19 22:51:14 -0600

    integracion de pwa

---
+++ docs/index.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker registrado', reg))
+      .catch(err => console.log('Error al registrar', err));
+  });
+}
